52e288dd08f885a4edf2a93b024da081c04d049c
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2022-06-14 15:24:21 -0700

    first version

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='not-found h-screen'>
+        <h1 className='text-3xl'>404 NotFound</h1>
+    </div>
+  )
+}
+
+export default NotFound
---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const register = () => {
+  return (
+    <div className='register h-screen'>
+         <h1 className='text-3xl'>Register</h1>
+    </div>
+  )
+}
+
+export default register
